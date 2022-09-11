76c68875cb9fc9292a9db8b74982395162c6a802
Seokju Na <seokju.me@gmail.com>
2022-09-12 01:32:42 +0900

    fix

--- packages/react-thermal-printer/src/utils/wrapText.spec.ts
+++ packages/react-thermal-printer/src/utils/wrapText.spec.ts
@@ -7,10 +7,25 @@ it('wrap text to multiple lines', () => {
     '자나깨나 불조심'; // 15
 
   expect(wrapText(text, { width: 8 })).toEqual([
-    '안녕하세',
-    '요반갑습',
-    '니다자나',
-    '깨나 불',
-    '조심',
+    {
+      length: 8,
+      text: '안녕하세',
+    },
+    {
+      length: 8,
+      text: '요반갑습',
+    },
+    {
+      length: 8,
+      text: '니다자나',
+    },
+    {
+      length: 7,
+      text: '깨나 불',
+    },
+    {
+      length: 4,
+      text: '조심',
+    },
   ]);
 });
