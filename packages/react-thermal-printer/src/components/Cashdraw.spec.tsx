abe334b83899f81120f9a512bc1ad79e09159fd3
Seokju Na <seokju.me@gmail.com>
2024-11-16 22:05:27 +0900

    support custom components inside `<Printer>` elem

--- packages/react-thermal-printer/src/components/Cashdraw.spec.tsx
+++ packages/react-thermal-printer/src/components/Cashdraw.spec.tsx
@@ -1,10 +1,11 @@
 import { getPrinter } from '@react-thermal-printer/printer';
+import { resetPrinter } from '../utils/resetPrinter';
 import { Cashdraw } from './Cashdraw';
 
 it('kick cash drawer', async () => {
   const actual = getPrinter({ type: 'epson' });
   const expected = getPrinter({ type: 'epson' });
 
-  Cashdraw.print(<Cashdraw pin="2pin" />, { printer: actual, width: 44 });
+  Cashdraw.print(<Cashdraw pin="2pin" />, { printer: actual, width: 44, reset: () => resetPrinter(actual) });
   expect(actual.getData()).toEqual(expected.cashdraw('2pin').getData());
 });
--- packages/react-thermal-printer/src/render.ts
+++ packages/react-thermal-printer/src/render.ts
@@ -1,5 +1,13 @@
-import { getPrinter } from '@react-thermal-printer/printer';
-import { Children, type ComponentProps, type ReactElement, type ReactNode, isValidElement } from 'react';
+import { type Printer as PrinterType, getPrinter } from '@react-thermal-printer/printer';
+import {
+  Children,
+  type ComponentClass,
+  type ComponentProps,
+  type FunctionComponent,
+  type ReactElement,
+  type ReactNode,
+  isValidElement,
+} from 'react';
 import type { Printer } from './components/Printer';
 import { isPrintable } from './types/Printable';
 import type { PrinterContext } from './types/PrinterContext';
@@ -7,17 +15,31 @@ import { resetPrinter } from './utils/resetPrinter';
 
 type PrinterProps = ComponentProps<typeof Printer>;
 
+export interface RenderOptions {
+  resetPrinter?: (printer: PrinterType) => void;
+}
+
 /**
  * Render the React element as printable binary data.
  */
-export async function render(elem: ReactElement<PrinterProps>): Promise<Uint8Array> {
-  const { type, characterSet, width = 48, encoder, initialize = true, debug = false, children } = elem.props;
+export async function render(elem: ReactElement<PrinterProps>, options?: RenderOptions): Promise<Uint8Array> {
+  const { type, characterSet, width = 48, encoder, initialize = true, debug = false } = elem.props;
   const printer = getPrinter({ type, characterSet, encoder });
 
   if (characterSet != null) {
     printer.setCharacterSet(characterSet);
   }
-  await print(children, { printer, width });
+  const context: PrinterContext = {
+    printer,
+    width,
+    reset() {
+      if (typeof options?.resetPrinter === 'function') {
+        return options.resetPrinter(printer);
+      }
+      return resetPrinter(printer);
+    },
+  };
+  await print(elem, context);
 
   if (initialize) {
     printer.initialize();
@@ -31,16 +53,27 @@ export async function render(elem: ReactElement<PrinterProps>): Promise<Uint8Arr
 }
 
 async function print(node: ReactNode, context: PrinterContext) {
-  for (const child of Children.toArray(node)) {
-    if (!isValidElement(child)) {
-      continue;
-    }
-
-    if (isPrintable(child.type)) {
-      await child.type.print(child, context);
-      resetPrinter(context.printer);
-    } else {
-      await print(child.props.children, context);
-    }
+  if (!isValidElement(node)) {
+    return;
   }
+  if (isPrintable(node.type)) {
+    await node.type.print(node, context);
+    resetPrinter(context.printer);
+  }
+  for (const child of renderReactElement(node)) {
+    await print(child, context);
+  }
+}
+
+function renderReactElement(elem: ReactElement) {
+  let rendered: ReactNode | ReactNode[] = [];
+  if (typeof elem.type === 'function') {
+    const isClassComp = !!elem.type.prototype && !!elem.type.prototype.isReactComponent;
+    rendered = isClassComp
+      ? new (elem.type as ComponentClass)(elem.props).render()
+      : (elem.type as FunctionComponent)(elem.props);
+  } else if (elem.props.children) {
+    rendered = elem.props.children;
+  }
+  return Children.toArray(rendered);
 }
