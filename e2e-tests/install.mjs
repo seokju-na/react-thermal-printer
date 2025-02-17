1b1c313e7952a1b1687c2a45d09b4dd073b01b89
Seokju Na <seokju.me@gmail.com>
2025-02-18 00:56:47 +0900

    fix

--- e2e-tests/install.mjs
+++ e2e-tests/install.mjs
@@ -1,18 +1,29 @@
+import { spawn } from 'node:child_process';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { execa } from 'execa';
 
 const rootDir = path.join(path.dirname(fileURLToPath(import.meta.url)), '..');
 
 await fs.rm(path.join(rootDir, 'e2e-tests', 'packages'), { recursive: true, force: true });
 await fs.mkdir(path.join(rootDir, 'e2e-tests', 'packages'), { recursive: true });
 
+async function run(command, args, options) {
+  return new Promise((resolve, reject) => {
+    const ps = spawn(command, args, {
+      ...options,
+      stdio: 'inherit',
+    });
+    ps.on('close', () => resolve());
+    ps.on('error', e => reject(e));
+  });
+}
+
 const packages = await fs.readdir(path.join(rootDir, 'packages'));
 for (const pkg of packages) {
   const pkgPath = path.join(rootDir, 'packages', pkg);
   console.log(`[${pkg}] yarn pack`);
-  await execa('yarn', ['pack'], { cwd: pkgPath, stdio: 'inherit' });
+  await run('yarn', ['pack'], { cwd: pkgPath });
   console.log(`[${pkg}] copy package tgz`);
   await fs.copyFile(path.join(pkgPath, 'package.tgz'), path.join(rootDir, 'e2e-tests', 'packages', `${pkg}.tgz`));
 }
