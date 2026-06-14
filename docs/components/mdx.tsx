8b40a7d31ff840f3735874fd40e9b3d433a84f6d
seokju-na <seokju.me@gmail.com>
2026-06-14 14:32:34 +0900

    docs: add auto type table (#129)
    * add auto type table
    
    * fix biome

--- docs/components/mdx.tsx
+++ docs/components/mdx.tsx
@@ -1,9 +1,16 @@
+import { createFileSystemGeneratorCache, createGenerator } from 'fumadocs-typescript';
+import { AutoTypeTable, type AutoTypeTableProps } from 'fumadocs-typescript/ui';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+
+const generator = createGenerator({
+  cache: createFileSystemGeneratorCache('.next/fumadocs-typescript'),
+});
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+    AutoTypeTable: (props: Partial<AutoTypeTableProps>) => <AutoTypeTable {...props} generator={generator} />,
     ...components,
   } satisfies MDXComponents;
 }
